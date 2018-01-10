a73781055c4d060d33c51751be3523a19ebf4224
Hot-m <allamouothmane@hotmail.fr>
2018-01-10 18:15:49 +0100

    cmd ended; shell in progress (it miss redirections)

--- shell_fct.c
+++
@@ -1,6 +0,0 @@
-#include "shell_fct.h"
-
-int exec_command(cmd* my_cmd){
-    //Your implementation comes here
-    return 0; //your return code goes here
-}
--- shell_fct.h
+++
@@ -1,8 +0,0 @@
-#include "cmd.h"
-//Your imports come here
-
-//Terminate shell
-#define MYSHELL_FCT_EXIT 1
-
-//Execute a command
-int exec_command(cmd *c);
